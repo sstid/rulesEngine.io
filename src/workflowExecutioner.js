7bdfc669bcb53275f352af31890fd65334243221
Ewald Ramp <ewald.ramp@sstid.com>
2021-05-28 14:01:32 -0400

    hasty

--- src/workflowExecutioner.js
+++ src/workflowExecutioner.js
@@ -1,4 +1,4 @@
-const cloneDeep = require('lodash.`clonedeep`');
+const cloneDeep = require('lodash.clonedeep');
 const { taskToShortName, taskToDescription } = require('./workflowSerialization');
 
 /**
